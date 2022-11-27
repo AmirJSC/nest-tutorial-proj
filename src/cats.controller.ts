999ca9d33312bbe6e0d3fde3ad0750a8af48d51d
Amir Jann Castaneda <amir@zap.com.ph>
2022-11-27 18:53:43 +0800

    Add cats controller

---
+++ src/cats.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, HttpCode, Post } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+  @Post()
+  @HttpCode(204)
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+}
